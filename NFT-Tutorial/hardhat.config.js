310b113f9507e0302d8ead9b39c3fb5ceaeafc9e
borntodie-new <2631937454@qq.com>
2023-02-26 11:11:33 +0800

    feat 第一个NFT

---
+++ NFT-Tutorial/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+// console.log(QUICKNODE_HTTP_URL);
+// console.log(PRIVATE_KEY);
+module.exports = {
+  solidity: "0.8.7",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
