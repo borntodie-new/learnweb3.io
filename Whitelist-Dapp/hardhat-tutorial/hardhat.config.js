4eeecf3545987fd392637aaa051a5358b265acea
jason <2631937454@qq.com>
2023-03-05 23:09:17 +0800

    feat 完成合约的部署

--- Whitelist-Dapp/hardhat-tutorial/hardhat.config.js
+++ Whitelist-Dapp/hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config({ path: ".env" });
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.7",
+  networks: {
+    goerli: {
+      url: ALCHEMY_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
 };
+// contract address 0x3c3A3FB2b03ac6bd81D4f782381a79362838EC3a
